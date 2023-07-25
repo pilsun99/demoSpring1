4e7d5f37e7cc94a3ad1644a8b11d54bd1c184910
DESKTOP-8467F69\ASUS <pilsun9599@gmail.com>
2023-07-25 19:38:40 +0700

    update

---
+++ src/main/java/niit/com/vn/demospring1/controller/ConfigResource.java
@@ -0,0 +1,17 @@
+package niit.com.vn.demospring1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigResource implements WebMvcConfigurer {
+    @Value("${UPLOAD_DIR}")
+    public String UPLOAD_FOLDER;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("upload/**").addResourceLocations("file:" + UPLOAD_FOLDER);
+    }
+}
--- src/main/java/niit/com/vn/demospring1/controller/UploadFileController.java
+++ src/main/java/niit/com/vn/demospring1/controller/UploadFileController.java
@@ -1,15 +1,23 @@
 package niit.com.vn.demospring1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.Calendar;
 
 @Controller("uploadFileController")
 public class UploadFileController {
+    @Value("${UPLOAD_DIR}")
+    public String uploadDir;
     @GetMapping("/upload-file")
     public String uploadFile(){
         return "upload-file";
@@ -18,11 +26,33 @@ public class UploadFileController {
 
     public  String uploadFileHandler(@RequestParam("img")MultipartFile file){
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\ASUS\\Downloads\\" + file.getOriginalFilename());
+            Calendar calander = Calendar.getInstance();
+            int year = calander.get(Calendar.YEAR); //lấy ve nam
+            int month = calander.get(Calendar.MONTH) + 1; // lấy về tháng
+            String newDir = month + "_" + year; //7_2023
+            //check xem thư mục đã tồn tại chưa
+            File folder = new File(uploadDir + newDir);
+            if (!folder.exists()|| folder.isFile()){
+                folder.mkdir();
+            }
+            FileOutputStream fileOutputStream = new FileOutputStream(uploadDir + newDir + File.separator + System.currentTimeMillis()+ file.getOriginalFilename());
             fileOutputStream.write(file.getBytes());
         }catch (Exception e){
             e.printStackTrace();
         }
         return "redirect:upload-file";
+    }
+
+    @GetMapping("/file")
+    public ResponseEntity<byte[]> serveFile(@RequestParam("file-name") String fileName){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(uploadDir + "7_2023/1690174035179Screenshot_20221221_111656.png");
+            byte[] bytes = new byte[fileInputStream.available()];
+            fileInputStream.read(bytes);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(bytes);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
